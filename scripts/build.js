eeb147b3045248adb5ca85c39091916e16a19c24
Umberto Toniolo <amountainram@gmail.com>
2022-07-19 20:54:09 +0200

    fix to routines

--- scripts/build.js
+++ scripts/build.js
@@ -1,36 +1,56 @@
 #!/usr/bin/env node
 import {build} from 'esbuild'
 import {
-  readFileSync, writeFileSync
-} from 'fs'
-import {
   dirname, resolve
 } from 'path'
 import {fileURLToPath} from 'url'
+import {writeFile} from 'fs/promises'
+import mkdirp from 'mkdirp'
 
-build({
-  platform: 'node',
-  entryPoints: ['./src/mgmt/index.ts', './src/create-lib/index.ts'],
-  outdir: 'dist',
-  minify: true,
-  bundle: true,
-  format: 'cjs',
-  // plugins: [
-  //   NodeResolvePlugin({
-  //     extensions: ['.ts', '.js'],
-  //     onResolved: (resolved) => resolved.includes('node_modules') ? ({external: true}) : resolved
-  //   }),
-  // ],
-  // banner: {js: '#!/usr/bin/env node'}
-}).then(() => {
-  const pos = dirname(fileURLToPath(import.meta.url))
-  const cl = readFileSync(resolve(pos, '../dist/create-lib/index.js')).toString()
-  const mgmt = readFileSync(resolve(pos, '../dist/mgmt/index.js')).toString()
-  writeFileSync(resolve(pos, '../dist/create-lib/index.js'), '#!/usr/bin/env node\n' + cl)
-  writeFileSync(resolve(pos, '../dist/mgmt/index.js'), '#!/usr/bin/env node\n' + mgmt)
-}).catch((err) => {
-  console.error(err)
-  process.exit(1)
-})
+const scriptDir = dirname(fileURLToPath(import.meta.url))
+const rootDir = resolve(scriptDir, '..')
+const srcDir = resolve(rootDir, 'src')
+
+const bundles = [
+  {
+    entryPoint: 'mgmt/index.ts',
+    outdir: './dist/mgmt'
+  },
+  {
+    entryPoint: 'create-lib/index.ts',
+    outdir: './dist/create-lib'
+  }
+];
+
+(async () => {
+  await Promise.all(bundles
+    .map(({
+      entryPoint, ...rest
+    }) => ({
+      platform: 'node',
+      format: 'cjs',
+      entryPoints: [resolve(srcDir, entryPoint)],
+      write: false,
+      minify: true,
+      bundle: true,
+      ...rest
+    }))
+    .map((opts) => build(opts))
+  ).then((results) =>
+    Promise.all(results.reduce((ps, {outputFiles}, i) => {
+      const {write} = bundles[i]
+      if (!write) {
+        outputFiles?.forEach(({
+          path, contents
+        }) => mkdirp(dirname(path)).then(() => ps.push(writeFile(path, '#!/usr/bin/env node\n' + Buffer.from(contents).toString()))))
+      }
+
+      return ps
+    }, []))
+  ).catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })
+})()
 
 
